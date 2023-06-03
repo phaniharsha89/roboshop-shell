25165593e116b003a72da386b3a9a7fcb520d298
phaniharsha89 <phaniharsha89@gmail.com>
2023-06-03 22:36:23 +0530

    sfsd

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ echo -e "\e[33m Install Redis  \e[0m"
 yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Update Redis Listen address \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf & /etc/redis/redis.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m start Redis service\e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
