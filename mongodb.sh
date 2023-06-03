b456a023def4395387af13774b0a6ea1a5348c26
phaniharsha89 <phaniharsha89@gmail.com>
2023-06-03 17:18:02 +0530

    sfsd

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,8 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
  
-# Modify the config file
+echo -e "\e[33mUpdate MongoDB Listen address\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf
 
 echo -e "\e[33mStart MongoDB Service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
