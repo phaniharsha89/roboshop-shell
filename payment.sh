09a6d3103ce7b6bb21487e7a7658b1836aa390b4
phaniharsha89 <phaniharsha89@gmail.com>
2023-06-04 23:43:49 +0530

    sfsd

---
+++ payment.sh
@@ -0,0 +1,25 @@
+echo -e "\e[33m Install Python \e[0m"
+yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Add Application User \e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[33m Create Application Directory \e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app 
+
+echo -e "\e[33m Download application content  \e[0m"
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
+cd /app 
+
+echo -e "\e[33m Extract application content\e[0m"
+unzip /tmp/payment.zip &>>/tmp/roboshop.log
+
+echo -e "\e[33m Download application Dependencies \e[0m"
+cd /app 
+pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
+
+echo -e "\e[33m start Payment service \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable payment &>>/tmp/roboshop.log
+systemctl restart payment &>>/tmp/roboshop.log
