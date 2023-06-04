d8c5349ebcfdd70debe650aec8dd550b09615b9f
phaniharsha89 <phaniharsha89@gmail.com>
2023-06-04 23:19:56 +0530

    sfsd

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,16 +1,16 @@
 echo -e "\e[33m Install Erlang Repos\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 
 echo -e "\e[33m Install Rebbitmq Repos\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 
 echo -e "\e[33m Install Rabbit MQ Server\e[0m"
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Start Rabbit MQ Service \e[0m"
-systemctl enable rabbitmq-server 
-systemctl start rabbitmq-server 
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log
+systemctl start rabbitmq-server &>>/tmp/roboshop.log
 
 echo -e "\e[33m Add Rabbit MQ Application user\e[0m"
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
