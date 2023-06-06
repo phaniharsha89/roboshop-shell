9af4c4e869332d776bea658150ce82b44fea83c1
phaniharsha89 <phaniharsha89@gmail.com>
2023-06-06 15:46:17 +0530

    sfsd

--- cart.sh
+++ cart.sh
@@ -1,34 +1,8 @@
+ source common.sh
+ component=cart
+
+ nodejs
  
-echo -e "\e[33m Configuring NodeJS Repos \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstall NodeJS \e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
-
-echo -e "\e[33m Add Application User \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
-echo -e "\e[33m Create Application Directory \e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app 
-
-echo -e "\e[33mDownload Application Content \e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
-cd /app 
-
-echo -e "\e[33mExtract Application Content \e[0m"
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
-cd /app 
-
-echo -e "\e[33mInstall NodeJS Dependencies \e[0m"
-npm install &>>/tmp/roboshop.log
-
-echo -e "\e[33m Setup systemd service  \e[0m"
-cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
-
-echo -e "\e[33m Restart Cart Service \e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable cart &>>/tmp/roboshop.log
-systemctl restart cart &>>/tmp/roboshop.log
 
 
