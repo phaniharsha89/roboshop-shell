e476c7c55f0657e923e84853729635f58f6a88bb
phaniharsha89 <phaniharsha89@gmail.com>
2023-06-10 18:24:58 +0530

    sfsd

--- common.sh
+++ common.sh
@@ -9,7 +9,7 @@ app_presetup () {
 echo -e "${color} Add Application User ${nocolor}"
 id roboshop &>>$log_file 
 if [$? -eq 1 ]; then 
-useradd roboshop &>>$log_file
+ useradd roboshop &>>$log_file
 fi
 if [ $? -eq 0 ]; then 
   echo SUCCESS
